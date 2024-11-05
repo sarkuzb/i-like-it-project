02d5a6a21573c9e5f3fb0459efa63e20c83e9822
sarkuzb <ssmartprinter@gmail.com>
2024-11-05 21:57:14 +0500

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [sendMessage, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    if (!sendMessage.trim()) return;
+    setMessages([...messages, sendMessage]);
+    setMessage("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg-gray-200 h-screen flex flex-col items-center justify-center">
+        <div className="flex flex-col border-2 border-slate-400 p-4 rounded-lg">
+          <input
+            className="outline-none p-1 mb-2 w-56"
+            value={sendMessage}
+            onChange={(e) => setMessage(e.target.value)}
+          ></input>
+          {messages.map((message, index) => (
+            <p
+              className="messages inline-block w-56 pl-1 my-2 border-b-2 border-t-2 border-slate-600 bg-gray-100 rounded font-light hover:bg-gray-200 hover:scale-105 transition transform"
+              key={index}
+            >
+              {message}
+            </p>
+          ))}
+          <button
+            className="p-1 bg-slate-500 text-gray-100 px-2 mt-2 rounded-lg font-semibold hover:bg-slate-600 active:bg-slate-500 active:scale-95 transition transform"
+            onClick={handleSend}
+          >
+            <span>Send It</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
